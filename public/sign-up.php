4dc2a0fbcf04453c1a703adc0a0b1795d9ed7bdf
Lim Cheng Qing <limchengqing@yahoo.com>
2018-11-25 14:02:06 -0800

    Made some changes on the html section of php

--- public/sign-up.php
+++ public/sign-up.php
@@ -128,13 +128,60 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 } // End of the main Submit conditional.
 ?>
-<h1>Register</h1>
-<form action="sign-up.php" method="post">
-    <p>Username: <input type="text" name="username" size="15" maxlength="20" value="<?php if (isset($_POST['username'])) echo $_POST['username']; ?>"></p>
-	<p>First Name: <input type="text" name="first_name" size="15" maxlength="20" value="<?php if (isset($_POST['first_name'])) echo $_POST['first_name']; ?>"></p>
-	<p>Last Name: <input type="text" name="last_name" size="15" maxlength="40" value="<?php if (isset($_POST['last_name'])) echo $_POST['last_name']; ?>"></p>
-	<p>Email Address: <input type="email" name="email" size="20" maxlength="60" value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>" > </p>
-	<p>Password: <input type="password" name="pass1" size="10" maxlength="20" value="<?php if (isset($_POST['pass1'])) echo $_POST['pass1']; ?>" ></p>
-	<p>Age in years: (optional): <input type="age" name="age" size="1" maxlength="10" value="<?php if (isset($_POST['age'])) echo $_POST['age']; ?>" ></p>
-	<p><input type="submit" name="submit" value="Register"></p>
-</form>
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+
+    <meta charset="utf-8">
+    <title>Recipe Database</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Questrial" rel="stylesheet">
+
+    <!-- This is a 3rd-party stylesheet for Font Awesome: http://fontawesome.io/ -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" media="screen">
+
+    <link rel="stylesheet" href="./style.css" media="screen">
+
+  </head>
+
+  <body>
+
+    <header>
+      <!-- The <i> tag below includes the bullhorn icon from Font Awesome -->
+      <a href="#"><h1 class="site-title"> The Recipe Database</h1></a>
+
+     <nav class="navbar">
+        <ul class="navlist">
+          <li class="navitem navlink active"><a href="index.html">Home</a></li>
+          <li class="navitem navlink"><a href="recipe.html">Recipe</a></li>
+          <li class="navitem navlink"><a href="create.html">Add a Recipe</a></li>
+		  <li class="navitem navlink"><a href="Search_recipe.html">Search a Recipe</a></li>
+		  <li class="navitem navlink"><a href="Delete_recipe.html">Delete or Update a Recipe</a></li>
+		  <li class="navitem navlink"><a href="log-in.php">Login</a></li>
+          <li class="navitem navlink"><a href="Sign-up.php">Register now</a></li>
+		  <li class="navitem navlink"><a href="About.html">About</a></li>
+        </ul>
+    </nav>	
+		
+	
+    </header>
+	<div class="container">
+      	   <div class="register-container">
+               <h1>Register</h1>
+
+               <form action="sign-up.php" method="post">
+                   <p>Username: <input type="text" name="username" size="15" maxlength="20" value="<?php if (isset($_POST['username'])) echo $_POST['username']; ?>"></p>
+                   <p>First Name: <input type="text" name="first_name" size="15" maxlength="20" value="<?php if (isset($_POST['first_name'])) echo $_POST['first_name']; ?>"></p>
+                   <p>Last Name: <input type="text" name="last_name" size="15" maxlength="40" value="<?php if (isset($_POST['last_name'])) echo $_POST['last_name']; ?>"></p>
+                   <p>Email Address: <input type="email" name="email" size="20" maxlength="60" value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>" > </p>
+
+                   <p>Password: <input type="password" name="pass1" size="10" maxlength="20" value="<?php if (isset($_POST['pass1'])) echo $_POST['pass1']; ?>" ></p>
+                   <p>Age in years: (optional): <input type="age" name="age" size="1" maxlength="10" value="<?php if (isset($_POST['age'])) echo $_POST['age']; ?>" ></p>
+                   <p><input type="submit" name="submit" value="Register"></p>
+               </form>
+                 </div>
+	   </div>
+               </body>
+      </html>
